571c1a499e9ce8efdc4ab3c208ecb2a473676473
Robby Findler <robby@racket-lang.org>
2017-02-18 14:29:48 -0600

    add a plant (using [ and ] thanks to new support in value-turtles)

---
+++ plant.rkt
@@ -0,0 +1,30 @@
+#lang s-exp "lang.rkt"
+
+(require graphics/value-turtles)
+
+(define (X turtles) turtles)
+(define (F turtles)
+  (cons (draw 3 (car turtles)) (cdr turtles)))
+(define (- turtles)
+  (cons (turn -22.5 (car turtles)) (cdr turtles)))
+(define (+ turtles)
+  (cons (turn 22.5 (car turtles)) (cdr turtles)))
+(define (|[| turtles)
+  (list* (car turtles) (turtle-state (car turtles)) (cdr turtles)))
+(define (|]| turtles)
+  (cons (restore-turtle-state (car turtles) (cadr turtles))
+        (cddr turtles)))
+
+(define w 400)
+(define h 500)
+
+(define start (cons (move (/ h -2) (turn 90 (move (/ w 10) (turtles w h)))) '()))
+(define (finish turtles) (clean (car turtles)))
+
+(l-system
+ #:convert-start start 
+ #:convert-finish finish
+ #:iterations 6
+ (X)
+ (X -> F - |[| |[| X |]| + X |]| + F |[| + F X |]| - X)
+ (F -> F F))
