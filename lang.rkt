e7f90958a53057e750489062b11f300bf63794c8
Robby Findler <robby@racket-lang.org>
2017-02-19 06:49:35 -0600

    refactor to pull out runtime a separate, explainable piece

--- lang.rkt
+++ lang.rkt
@@ -4,17 +4,9 @@
    syntax/parse
    syntax/id-table
    racket/dict
-   racket/base))
+   racket/base)
+  "runtime.rkt")
 (provide l-system)
-
-#|
-;; an exp is either:
-;;   - symbol?
-;;   - (listof container)
-|#
-
-;; content : exp
-(struct container (content) #:mutable #:transparent)
 
 (define-for-syntax non-terminals (make-hash))
 
@@ -25,19 +17,13 @@
        (hash-set! non-terminals (hash-count non-terminals)
                   (syntax-local-introduce name)))
      #'(void)]))
+
 (define-syntax (rule stx)
   (syntax-parse stx
-    [(_ lhs rhs ...)
+    [(_ rhs ...)
      (define ids (for/list ([i (in-range (hash-count non-terminals))])
                    (syntax-local-introduce (hash-ref non-terminals i))))
-     #`(λ (lhs)
-         (define to-mutate lhs)
-         (λ (#,@ids)
-           (set-container-content! to-mutate (list rhs ...))))]))
-(define-syntax (axiom stx)
-  (syntax-parse stx
-    [(_ root a ...)
-     #'(define root (container (list a ...)))]))
+     #`(λ (#,@ids) (list rhs ...))]))
 
 (define-syntax (get-non-terminals stx)
   #`(list #,@(for/list ([i (in-range (hash-count non-terminals))])
@@ -50,11 +36,10 @@
        #'(let ([A (container (add-prefix A))] ...
                [T (container (add-prefix T))] ...)
            (register-non-terminals A ...)
-           (define rules (list (rule A B ...) ...))
-           (axiom root C ...)
-           (define non-terminals-box (box (get-non-terminals)))
-           (run-it root rules non-terminals-box
-                   start finish variables)))]))
+           (run-lindenmayer (container (list C ...))
+                            (get-non-terminals)
+                            (list (rule B ...) ...)
+                            start finish variables)))]))
 
 (define-syntax (add-prefix stx)
   (syntax-parse stx
@@ -74,26 +59,3 @@
       (free-id-table-set! result candidate #t)))
   (for/list ([(id _) (in-dict result)])
     id))
-
-(define (rewrite rules non-terminals-box)
-  (define new-non-terminals
-    (for/list ([non-terminal (in-list (unbox non-terminals-box))])
-      (container (container-content non-terminal))))
-  (for ([rule (in-list rules)]
-        [non-terminal (in-list (unbox non-terminals-box))])
-    (apply (rule non-terminal) new-non-terminals))
-  (set-box! non-terminals-box new-non-terminals))
-
-(define (render-it root start finish variables)
-  (define current (start variables))
-  (let loop ([ele root])
-    (cond
-      [(container? ele) (loop (container-content ele))]
-      [(list? ele) (for ([ele (in-list ele)]) (loop ele))]
-      [(procedure? ele) (set! current (ele current variables))]))
-  (finish current variables))
-
-(define (run-it root rules non-terminals-box start finish variables)
-  (for ([i (in-range (hash-ref variables 'n 4))])
-    (rewrite rules non-terminals-box))
-  (printf "~a\n" (render-it root start finish variables)))
---
+++ runtime.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(provide container run-lindenmayer)
+
+#|
+;; an Exp α is either:
+;;   - (-> α hash α)
+;;   - (listof (Container α))
+|#
+
+;; content : Exp α
+(struct container (content) #:mutable #:transparent)
+
+(define (run-lindenmayer root non-terminals rules start finish variables)
+  (define non-terminals-box (box non-terminals))
+  (for ([i (in-range (hash-ref variables 'n 4))])
+    (rewrite non-terminals-box rules))
+  (printf "~a\n" (render-it root start finish variables)))
+
+(define (rewrite non-terminals-box rules)
+  (define new-non-terminals
+    (for/list ([non-terminal (in-list (unbox non-terminals-box))])
+      (container (container-content non-terminal))))
+  (for ([rule (in-list rules)]
+        [non-terminal (in-list (unbox non-terminals-box))])
+    (set-container-content! non-terminal (apply rule new-non-terminals)))
+  (set-box! non-terminals-box new-non-terminals))
+
+(define (render-it root start finish variables)
+  (define current (start variables))
+  (let loop ([ele root])
+    (cond
+      [(container? ele) (loop (container-content ele))]
+      [(list? ele) (for ([ele (in-list ele)]) (loop ele))]
+      [(procedure? ele) (set! current (ele current variables))]))
+  (finish current variables))
