1860bddce06e05d529bf67f1b1a4b4ac53306147
Robby Findler <robby@racket-lang.org>
2017-02-23 17:59:47 -0600

    correct for pen width issues when scaling the picts

--- turtle.rkt
+++ turtle.rkt
@@ -20,4 +20,11 @@
   (define h (hash-ref variables 'h 500))
   (cons (move (/ h -2) (turn 90 (move (/ w 10) (turtles w h)))) '()))
 (define (finish turtles variables)
-  (scale-to-fit (turtles-pict (car turtles)) 300 300))
+  (define orig (turtles-pict (car turtles)))
+  (define scaled (scale-to-fit orig 300 300))
+  (define scale-factor (/ (pict-width scaled) (pict-width orig)))
+  ;; when the pict is scaled, lines that are drawn with width 1
+  ;; will now be drawn at the width `scale-factor`, so we inset
+  ;; the pict by 1/2 of that for the bits of lines that'll be
+  ;; hanging off the edge of the pict
+  (inset scaled (* 1/2 scale-factor)))
