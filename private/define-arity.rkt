e8b1316032a6db49914b964ccefda9f328c3e5e2
Robby Findler <robby@racket-lang.org>
2017-02-23 21:52:36 -0600

    code tidying for define/arity

--- private/define-arity.rkt
+++ private/define-arity.rkt
@@ -2,28 +2,34 @@
 (require (for-syntax racket/base syntax/parse))
 (provide define/arity)
 
+(define-for-syntax (signal-length-error formal-args-length actual-args-length def-site-f use-site-f)
+  (define f (syntax-e def-site-f))
+  (raise-syntax-error f
+                      (format
+                       "expected ~a arguments for ~a, found ~a"
+                       formal-args-length
+                       f
+                       actual-args-length)
+                      #f
+                      use-site-f
+                      (list def-site-f)))
+
 (define-syntax (define/arity stx)
   (syntax-parse stx 
-    [(_ (f formal:id ...) e)
+    [(_ (def-site-f formal:id ...) e)
      #`(begin
-         (define (f-proc formal ...) e)
-         (define-syntax f
-           (λ (stx)
-             (syntax-parse stx
-               [(the-f actual (... ...))
-                (define actual-args-length (length (syntax->list #'(actual (... ...)))))
-                (define formal-args-length #,(length (syntax->list #'(formal ...))))
-                (unless (= actual-args-length formal-args-length)
-                  (raise-syntax-error 'f
-                                      (format
-                                       "expected ~a arguments for ~a, found ~a"
-                                       formal-args-length
-                                       'f
-                                       actual-args-length)
-                                      #f
-                                      #'the-f
-                                      (list #'f)))
-                #'(f-proc actual (... ...))]))))]))
+         (define-syntax (def-site-f stx)
+           (syntax-parse stx
+             [(use-site-f actual (... ...))
+              (define actual-args-length
+                (length (syntax->list #'(actual (... ...)))))
+              (define formal-args-length
+                #,(length (syntax->list #'(formal ...))))
+              (unless (= actual-args-length formal-args-length)
+                (signal-length-error formal-args-length actual-args-length
+                                     #'definition-site-f #'use-site-f))
+              #'(f-proc actual (... ...))]))
+         (define (f-proc formal ...) e))]))
 
 (module+ test
   (require rackunit)
