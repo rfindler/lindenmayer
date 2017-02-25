c9b3881ad699806190909daa20eeb27ab08d47b4
shhyou <shuhung.you@eecs.northwestern.edu>
2017-02-24 22:10:03 -0600

    Alternating leaves

---
+++ examples/leaf-alternating.rkt
@@ -0,0 +1,70 @@
+#lang lindenmayer racket
+
+## axiom ##
+A(0)
+
+## rules ##
+A(d) : d > 0 -> A(d+s)
+A(d) : d = 0 -> F(1)[+A(D)]F(1)B(0)
+B(d) : d > 0 -> B(d+s)
+B(d) : d = 0 -> F(1)[-B(D)]F(1)A(0)
+F(a)         -> F(a*R)
+
+## variables ##
+D=1
+R=1.36
+
+n=20
+
+s=-1
+θ=45
+
+------------------------------------------------------------
+
+## axiom ##
+A(0)
+
+## rules ##
+A(d) : d > 0 -> A(d+s)
+A(d) : d = 0 -> F(1)[+A(D)]F(1)B(0)
+B(d) : d > 0 -> B(d+s)
+B(d) : d = 0 -> F(1)[-B(D)]F(1)A(0)
+F(a)         -> F(a*R)
+
+## variables ##
+D=4
+R=1.18
+
+n=34
+
+s=-1
+θ=45
+
+------------------------------------------------------------
+
+## axiom ##
+A(0)
+
+## rules ##
+A(d) : d > 0 -> A(d+s)
+A(d) : d = 0 -> F(1)[+A(D)]F(1)B(0)
+B(d) : d > 0 -> B(d+s)
+B(d) : d = 0 -> F(1)[-B(D)]F(1)A(0)
+F(a)         -> F(a*R)
+
+## variables ##
+D=7
+R=1.13
+
+n=46
+
+s=-1
+θ=45
+
+============================================================
+;; Section 5.3 Models of compound leaves
+;; 130/142 Figure 5.12: Compound leaves with alternating branching patterns
+(require lindenmayer/turtle)
+(provide (all-from-out lindenmayer/turtle) (all-defined-out))
+(define (A turtles variables . _) turtles)
+(define (B turtles variables . _) turtles)
