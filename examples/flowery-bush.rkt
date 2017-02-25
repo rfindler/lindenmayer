3b15d5357a05b39322679343d5ae27b3f276315c
Spencer Florence <spencer@florence.io>
2017-02-24 23:16:01 -0600

    finish the flowery bush!

--- examples/flowery-bush.rkt
+++ examples/flowery-bush.rkt
@@ -3,26 +3,27 @@
 ## axiom ##
 P
 ## rules ##
-P → I+[P+F]--//[--L]I[++L]-[PF]++PF
+P → I+[P+G]--//[--L]I[++L]-[PG]++PG
 I → FS[//&&L][//^^]LFS
 S → SFS
 L → ['{+f-ff-f+|+f-ff-f}]
-F → [&&&p'/W////W////W////W////W////]
+G → [&&&p'/W////W////W////W////W////]
 p → FF
 W → ['^F][''{&&&&-f+f|-f+f}]
 ## variables ##
 n=5
 δ=18
-w=500
-h=500
-dist=20
-rot=45
+w=2000
+h=2000
+dist=35
+rot=-20
 =========================
 
 #|
 P is the plant
 S is stem
 E is segment
+G is flower
 |#
 (provide (all-defined-out)
          (all-from-out "linden-3d-turtle.rkt"))
@@ -31,19 +32,19 @@ E is segment
          (except-in pict3d move))
 
 (define (P state vars) state)
+(define (G state vars) state)
 (define (I state vars) state)
 (define (S state vars) state)
 (define (L state vars) state)
 (define (p state vars) state)
 (define (W state vars) state)
 
-(define (start vars) (make-turtle (dir 0 0 -5) +z +x))
+(define (start vars) (make-turtle (dir 0 -10 -40) +z +x))
 
 
 (define (finish turtles variables)
-    
   (define dist (hash-ref variables 'dist))
-  (define v (angles->dir (hash-ref variables 'rot) 0))
+  (define v (angles->dir (hash-ref variables 'rot) 10))
   (define camera (basis 'camera (point-at (pos+ origin (dir-scale v dist)) (pos 0 0 .01) #:up +x)))
 
   (set-rendering-config!
