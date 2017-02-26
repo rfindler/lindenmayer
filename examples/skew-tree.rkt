6d619d54696c4c2a68b30312fc3082f47ee6f827
shhyou <shuhung.you@eecs.northwestern.edu>
2017-02-25 18:28:19 -0600

    Add two skew fern-like trees

---
+++ examples/skew-tree.rkt
@@ -0,0 +1,53 @@
+#lang lindenmayer racket
+
+## axiom ##
+
+A(1)
+
+## rules ##
+
+A(s) → F(l*s) [ -(5) A(s*c) ] [ +(1) A(s*c) ]
+
+## variables ##
+
+l = 94
+c = 0.7
+θ = 10
+
+n = 12
+------------------------------------------------------------
+
+## axiom ##
+
+A(1)
+
+## rules ##
+
+A(s) → F(l*s) [ -(5) B(s*c) ] [ +(1) D(s*c) ]
+B(s) → F(l*s) [ +(5) A(s*c) ] [ -(1) C(s*c) ]
+C(s) → F(l*s) [ -(5) E(s*c) ] [ +(1) A(s*c) ]
+D(s) → F(l*s) [ +(5) B(s*c) ] [ -(1) E(s*c) ]
+E(s) → F(l*s) [ -(5) A(s*c) ] [ +(1) C(s*c) ]
+
+## variables ##
+
+l = 100
+c = 0.7
+θ = 10
+
+n = 12
+
+============================================================
+;; Examples taken from Winfried Kurth, Specification of morphological models with
+;; L-systems and relational growth grammars, Journal of Interdisciplinary Image Science
+;;
+;; The first L-system is Fig. 6(a) in the paper (page 7) and the second L-system is a
+;; variation of the L-system in Fig. 6(b), changing the system to be deterministic instead.
+(require lindenmayer/turtle)
+(provide (all-from-out lindenmayer/turtle) (all-defined-out))
+
+(define (A turtles variables . _) turtles)
+(define (B turtles variables . _) turtles)
+(define (C turtles variables . _) turtles)
+(define (D turtles variables . _) turtles)
+(define (E turtles variables . _) turtles)
