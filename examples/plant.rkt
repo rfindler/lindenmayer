9dcc004fd621e14023e7e88c380223e4c46a9643
shhyou <shuhung.you@eecs.northwestern.edu>
2017-02-25 20:26:00 -0600

    A furry plant example

--- examples/plant.rkt
+++ examples/plant.rkt
@@ -85,8 +85,48 @@ n=6
 w=350
 h=350
 
+------------------------------------------------------------
+
+## axiom ##
+
+S L F(p)
+
+## rules ##
+
+S -> [+++G] [---H] T S
+
+G -> +H [-G] L
+H -> -G [+H] L
+
+T -> UL
+U -> V
+V -> TL
+
+L -> [-F(p)] [+F(p)] F(q)
+
+## variables ##
+
+p=1
+q=7
+n=12
+θ=18
+
 ============================================================
+;; The first four examples are from ABOP p.25
+;;
+;; The last example is modified from Przemyslaw Prusinkiewicz,
+;; Graphical applications of L-systems, Proceedings of Graphics
+;; Interface '86, Fig. 3(f)
+
 (require lindenmayer/turtle)
 (provide (all-from-out lindenmayer/turtle) (all-defined-out))
 (define (X turtles variables) turtles)
+(define (S turtles variables) turtles)
+(define (R turtles variables) turtles)
+(define (G turtles variables) turtles)
+(define (H turtles variables) turtles)
+(define (T turtles variables) turtles)
+(define (U turtles variables) turtles)
+(define (V turtles variables) turtles)
+(define (L turtles variables) turtles)
 
