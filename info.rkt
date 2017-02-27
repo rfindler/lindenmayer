9457d92a47cfff7aa747f83f55f091247c4aaba9
Robby Findler <robby@racket-lang.org>
2017-02-26 21:56:08 -0600

    add info spec

--- info.rkt
+++ info.rkt
@@ -1,3 +1,16 @@
 #lang info
 (define drracket-tools '(("tool.rkt")))
 (define drracket-tool-names (list "L-System Refactor"))
+
+(define deps '("base"
+               "data-lib"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "htdp-lib"
+               "parser-tools-lib"
+               "pict-lib"
+               "pict3d"
+               "syntax-color-lib"
+               "typed-racket-lib"))
+(define build-deps '("2d-lib"
+                     "rackunit-lib"))
